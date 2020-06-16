1f6bae29d8c729e8cee52b7e7a9735c01d765373
Your Name <you@example.com>
2020-06-16 15:09:42 +0530

    sample

---
+++ ds.js
@@ -0,0 +1,40 @@
+// var a= [];
+// var b= "bob";
+// var c = "";
+
+// for (var i =0; i<b.length; i++){
+//     a.push(b[i]);
+// }
+
+// for(var i=0; i<b.length; i++){
+//     b += a.pop();
+
+// }
+ 
+// if(b===c) {
+//     console.log(c + "is a pan")
+// }
+
+let a=[1,2,3,4,5];
+    let first;
+let b = a.length;
+n=4;
+for(var i=0; i<n; i++){
+
+    first=a[0];
+
+for(var j=0; j<b-1; j++){
+    a[j]=a[j+1];
+}
+
+
+first=a[j];
+
+
+
+}
+for(vari=0; i<b; i++){
+console.log(a[i]);
+}
+
+
